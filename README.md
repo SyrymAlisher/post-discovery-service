00f5407873548f5c450695d155d170b3aff755ed
SyrymAlisher <syrymalisher@gmail.com>
2022-03-27 23:01:23 +0600

    commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# post-discovery-service" 
---
+++ src/test/java/dar/intern/postdiscoveryservice/PostDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dar.intern.postdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
